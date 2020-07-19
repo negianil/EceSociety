07a251cd8833e4aa9423394094adc007098497aa
Shyam Singhania <shyamsinghania4@gmail.com>
2020-07-19 22:43:40 +0530

    Updates

---
+++ precache-manifest.468f6566adea51c35b34a32c2676afd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff0f8459396d65fe690e3eeb7021291",
+    "url": "/EceSociety/index.html"
+  },
+  {
+    "revision": "dc1ffed768badf0f8c88",
+    "url": "/EceSociety/static/js/2.ae3603ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/EceSociety/static/js/2.ae3603ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5162518fee84b2aaa4",
+    "url": "/EceSociety/static/js/main.91c7d613.chunk.js"
+  },
+  {
+    "revision": "45cebe2041bed144fc37",
+    "url": "/EceSociety/static/js/runtime-main.e603e23a.js"
+  }
+]);
